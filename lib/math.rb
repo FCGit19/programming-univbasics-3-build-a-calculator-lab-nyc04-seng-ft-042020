4168c1f306308e418b2907eee94d37e2cdf10f4c
Fred C <canofred@gmail.com>
2020-03-27 02:31:25 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,28 +1,29 @@
 def addition(num1, num2)
-sum = num1 + num2
-return sum
+  sum = num1 + num2
+  return sum
 end
 
 def subtraction(num1, num2)
-difference = num1 - num2
-return difference 
+  difference = num1 - num2
+  return difference 
 end
 
 def division(num1, num2)
-quotient = num1 / num2
-return quotient
+  quotient = num1 / num2
+  return quotient
 end
 
 def multiplication(num1, num2)
-product = num1 * num2
-return product
+  product = num1 * num2
+  return product
 end
 
 def modulo(num1, num2)
-modulo = num1 % num2
-return modulo
+  modulo = num1 % num2
+  return modulo
 end
 
 def square_root(num)
-
+  sqrt = num * num 
+  return sqrt 
 end
